2fd049694caf0bc238ca3a40a0eef2c185ea0814
Tim <timgomperts@gmail.com>
2025-02-26 07:46:40 +0100

    Updated css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CalculadoraComponent],
+  imports: [CalculadoraComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
